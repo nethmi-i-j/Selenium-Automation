2ac583303e0d0dbdaa98bf5262d330cf331f9e9b
Nethmi <nethmijayasinghe31@gmail.com>
2025-07-09 15:38:09 +0530

    click (inside nested frame)

--- src/test/java/FrameExample.java
+++ src/test/java/FrameExample.java
@@ -21,12 +21,31 @@ public class FrameExample {
 
         // 1) click (inside frame)
 
-        driver.switchTo().frame(0);
+        driver.switchTo().frame(0);  // in frame 1
         WebElement button1 = driver.findElement(By.xpath("//button[@id='Click']"));
         button1.click();
 
         String clickButton1 = button1.getText();
         System.out.println("after click inside frame button text: "+clickButton1);
+
+        //Click (inside nested frame)
+
+        /* there are 4 frames in this web page. now we are in frame 1. to move in to the frame 4which is inside in the
+        frame 3, we should first enter the main html page and then move in to the frame 3 then after head in to
+        the frame 4 (Nested frame) */
+
+        driver.switchTo().defaultContent();  // switch to the main html page
+
+        driver.switchTo().frame(2);  // switch to third frame
+
+        driver.switchTo().frame(0);  // since only a one frame inside the fourth frame
+
+        WebElement buttton3 = driver.findElement(By.id("Click"));
+        buttton3.click();
+        String clickButton3 = buttton3.getText();
+        System.out.println("After click inside nested frame button text: "+clickButton3);
+
+
 
 
 
