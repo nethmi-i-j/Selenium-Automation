f64b004f8bc2d0c4f137a095ed2aa4db1933eb77
Nethmi <nethmijayasinghe31@gmail.com>
2025-07-23 13:22:46 +0530

    Keyboard actions (keys: control, shift)

---
+++ src/test/java/KeyBoardActions.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class KeyBoardActions {
+
+    WebDriver driver;
+    @BeforeMethod
+
+    public void KeyBoardPage(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void KeyBoardTest1(){
+
+        driver.get("");
+        WebElement textBox = driver.findElement(By.name("q"));
+        textBox.sendKeys("welcome");
+
+        Actions actions = new Actions(driver);
+        // select the text
+        Action storeBuildOperation = actions.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build();  //.build().perform() = .perform()
+        storeBuildOperation.perform();
+
+        actions.keyDown(Keys.SHIFT)  //typing capital letters
+                .sendKeys("writing capital sentence")
+                .keyUp(Keys.SHIFT)
+                .keyDown(Keys.CONTROL)  // select the sentence
+                .sendKeys("a")
+                .keyUp(Keys.CONTROL)
+                .keyDown(Keys.CONTROL)  //delete the selected sentence
+                .sendKeys("x")
+                .perform();
+
+        // to write capital in a textbox
+        actions.keyDown(textBox,Keys.SHIFT).sendKeys("hello").perform();
+
+    }
+}
